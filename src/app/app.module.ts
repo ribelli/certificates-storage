39e5ea08a10e1fae5a80270a2cc171ba9c78cf5a
ribelli <ribelli.oi@gmail.com>
2020-05-12 00:08:26 +0300

    replace app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {CSAComponentsModule} from './components/components.module';
-import {AppComponent} from './components/app.component';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material';
